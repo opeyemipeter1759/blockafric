a3fd32194db3c6a840d8547642c845e173646af1
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-04-17 13:36:16 -0700

    header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./header.css";
+import Avatar from "@material-ui/core/Avatar";
+import logo from "./assests/logo.png";
+
+function Header() {
+  return (
+    <div className="header">
+      {/* logo */}
+
+      <div className="logo-left">
+        <img src={logo} alt="logo- img" />
+        <p className="logo-text">ribbon</p>
+      </div>
+      <div className="logo-right">
+        <Avatar />
+        <i class="fas fa-bell"></i>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
