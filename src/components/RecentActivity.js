52e19adde015df64858a9744a1b1026debf46f13
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-04-19 11:57:15 -0700

    recent activity

--- src/components/RecentActivity.js
+++ src/components/RecentActivity.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./recentactivity.css";
-import { bp } from "./assests/bp.png";
-import { hiv } from "./assests/hiv.png";
-import { xray } from "./assests/xray.jpg";
+import bp from "./assests/bp.png";
+import hiv from "./assests/hiv.png";
+import xray from "./assests/xray.jpg";
 
 function RecentActivity() {
   return (
@@ -16,6 +16,21 @@ function RecentActivity() {
         <p>HIV Blood Test</p>
 
         <p className="a100">A100</p>
+      </div>
+      <div className="hiv-session">
+        <img src={hiv} alt="hiv" />
+        <p>HIV Blood Test</p>
+
+        <p className="a100">A100</p>
+      </div>
+      <div className="hiv-session">
+        <img src={hiv} alt="hiv" />
+        <p>HIV Blood Test</p>
+
+        <p className="a100">A100</p>
+      </div>
+
+      <div className="recent-end">
       </div>
     </div>
   );
