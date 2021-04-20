29b5fcacf5c92d2681853e2432ac7eb8e6d55f5a
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-04-19 19:49:18 -0700

    fix

--- src/components/SelectCurrency.js
+++ src/components/SelectCurrency.js
@@ -8,15 +8,40 @@ function SelectCurrency() {
         <p>Select Currency:</p>
       </div>
 
-      <div className="select-area">
+      {/* <div className="select-area">
         <div className="dropdown">
           <p className="dropdown-text"> AFYA</p>
-          <i class="fas fa-angle-down"></i>
+          <i className="fas fa-angle-down"></i>
         </div>
 
         <div className="figure">
           <p className="">0.000000001</p>
         </div>
+      </div> */}
+
+      <div className="input-group mb-3 mt-3">
+        <div className="input-group-prepend">
+          <button
+            className="btn btn-outline-secondary dropdown-toggle"
+            type="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            AFYA
+          </button>
+          <div className="dropdown-menu">
+            <a className="dropdown-item" href="#">
+              ZYA
+            </a>
+          </div>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Text input with dropdown button"
+          placeholder="0.000000001"
+        />
       </div>
       <div className="but">
         <Button variant="contained">SEND</Button>
