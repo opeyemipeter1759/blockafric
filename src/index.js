b0bc095559379cd301685aa49970bae8c1e21914
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-04-19 20:28:39 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import $ from "jquery";
-import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";  
 import "bootstrap/dist/css/bootstrap.min.css";  
 import App from './App';
