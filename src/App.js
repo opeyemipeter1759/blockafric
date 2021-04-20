10ea272e9ab9219566b35855475373a8e97c4a3f
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-04-19 17:38:36 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Activity from "./Pages/Activity";
-import Home from "./Pages/Home";
 import Market from "./Pages/Market";
 import Earn from "./Pages/Earn";
 import Wallet from "./Pages/Wallet";
@@ -13,9 +12,7 @@ function App() {
     <BrowserRouter>
       <div className="app">
         <Switch>
-          <HomePage />
-
-          <Route path="/home" exact component={Home} />
+          <Route path="/home" exact component={HomePage} />
           <Route path="/activity" exact component={Activity} />
           <Route path="/market" exact component={Market} />
           <Route path="/earn" exact component={Earn} />
